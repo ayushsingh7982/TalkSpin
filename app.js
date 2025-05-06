4d919d9da61a9e1fe87a76ca1fa019e2a6af5200
Ayush singh <143987677+ayushsingh7982@users.noreply.github.com>
2025-05-06 19:36:22 +0530

    Home page done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const socket=require("socket.io");
+const http=require("http");
+const path=require("path");
+
+const app=express();
+const port=4000;
+
+const server = http.createServer(app);
+const io = socket(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/",(req,res)=>{
+    res.render("home");
+    // res.send("Working");
+})
+
+server.listen(port,()=>{
+    console.log(`App is running on ${port}`);
+})
